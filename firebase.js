cd737534e478d9c25dbda97c984af47f64fcd5ee
LuxM-pixel <Mona.Alshomrani27@outlook.sa>
2026-06-27 11:54:27 +0300

    Initialize Firebase app and Firestore database

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDEjf_HrBdJSn1EVaHeyvMubUkPjVZH5i0",
+  authDomain: "tdrive-4fed8.firebaseapp.com",
+  projectId: "tdrive-4fed8",
+  storageBucket: "tdrive-4fed8.firebasestorage.app",
+  messagingSenderId: "107257320648",
+  appId: "1:107257320648:web:30d142684f936a374041fd",
+  measurementId: "G-QBEPEKERXV"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.db = db;
